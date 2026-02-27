057b9a04f6f95c8c25396e678db905db636df1cb
DipeshWadhwani <wadhwanidipesh53@gmail.com>
2026-02-27 17:46:43 +0530

    learn pandas

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv("pokemon.csv")
-print(df)
+# print(df)  # this will add first 5 and last five in the terminal when you will run.
 
+# to show everything in the terminal .to_string() is used
+
+print(df.to_string()) # this will show every row every column not the truncated version
