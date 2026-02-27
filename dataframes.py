c6c773fd2053cd83db841b3f65c16a368d34f6da
DipeshWadhwani <wadhwanidipesh53@gmail.com>
2026-02-27 21:38:25 +0530

    DataFrames in pandas

--- dataframes.py
+++ dataframes.py
@@ -8,5 +8,20 @@ family = {
     "Age" : [18,13,42,52,30,34,49,55]
 }
 
-df = pd.DataFrame(family)
+df = pd.DataFrame(family, index=[1,2,3,4,5,6,7,8])
+print(df)
+
+# add column 
+df["Job"] = ["Student","Student","Housewife","Businessman","Job","Job","Housewife","Businessman"]
+print(df)
+
+# add row
+
+new_members = pd.DataFrame([{"Name": "Sneha","Age": 29,"Job" : "Housewife"},{"Name" : "Vrishti","Age" : 3,"Job": "Kid"}], index = [9,10])
+df = pd.concat([df,new_members])
+
+print(df)
+
+#this is another way to add row
+df.loc[11] = ["Monishka", 17, "Student"] # this will add row with index 11 and the values in the list   
 print(df)
